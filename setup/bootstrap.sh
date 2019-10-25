9f632aa65219fb2064505164b2f8eb42db6252a9
Jereme <michael@jeremehancock.com>
2019-10-25 07:26:54 -0500

    updated build

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -53,11 +53,11 @@ sudo -E a2enmod rewrite
 echo -e "\e[32mEnable mod_rewrite Complete\e[0m"
 
 # Update Apache conf
-sudo -E echo '<Directory /var/www/html>' >> /etc/apache2/sites-available/000-default.conf
-sudo -E echo '    Options Indexes FollowSymLinks' >> /etc/apache2/sites-available/000-default.conf
-sudo -E echo '    AllowOverride All' >> /etc/apache2/sites-available/000-default.conf
-sudo -E echo '    Require all granted' >> /etc/apache2/sites-available/000-default.conf
-sudo -E echo '</Directory>' >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo "<Directory /var/www/html>" >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo "    Options Indexes FollowSymLinks" >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo "    AllowOverride All" >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo "    Require all granted" >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo "</Directory>" >> /etc/apache2/sites-available/000-default.conf
 
 echo -e "\e[32mUpdated Apache conf Complete\e[0m"
 
